f4ce91bacb191208f5d403b0e9d5731eae47e1e8
Pritam Barua <pritambarua@Pritams-MacBook-Air.local>
2022-06-07 21:10:26 +0530

    remove critical info

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCs9eMdJOFy8Q960SDTh-no00P48HVsTkw",
-    authDomain: "preet-tac-toe.firebaseapp.com",
-    projectId: "preet-tac-toe",
-    storageBucket: "preet-tac-toe.appspot.com",
-    messagingSenderId: "576351789254",
-    appId: "1:576351789254:web:24596f6ff182cf7180b7c7",
-    measurementId: "G-KEQ2D5HGBK"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
   const app = initializeApp(firebaseConfig);
